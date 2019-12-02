d161ec9d5ec781c14e4911e64b988da6e292b6e7
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-12-02 14:00:40 -0600

    hard code

--- Assets/Scripts/keyboardClass.cs
+++ Assets/Scripts/keyboardClass.cs
@@ -12,7 +12,7 @@ public class keyboardClass : MonoBehaviour, ISelectHandler {
     public string promptHeader;
 
 	[DllImport("__Internal")]
-	private static extern void focusHandleAction (string _name, string _str, string _promptHeader);
+	private static extern void focusHandleAction (string _name, string _str);
 
 	public void ReceiveInputData(string value) {
 		gameObject.GetComponent<TMP_InputField> ().text = value;
@@ -21,7 +21,7 @@ public class keyboardClass : MonoBehaviour, ISelectHandler {
 	public void OnSelect(BaseEventData data) {
 		#if UNITY_WEBGL
 		try{
-			focusHandleAction (gameObject.name, gameObject.GetComponent<TMP_InputField> ().text, promptHeader);
+			focusHandleAction (gameObject.name, gameObject.GetComponent<TMP_InputField> ().text);
 		}
 		catch(Exception error){}
 		#endif
